d058dff2a039f3720b2fde975683dacdcf50a692
tvan <tomvan.dev@gmail.com>
2023-08-11 22:21:54 -0400

    minor http_server tweaks for timezone and output

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,4 +1,4 @@
 module.exports.getDate = function getDate() {
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
+    let aestTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
     return new Date(aestTime);
 }
